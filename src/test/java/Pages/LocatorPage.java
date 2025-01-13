dc77c0b1ddc9c26ff7312c6aded2f3562454139d
kudencagri <Kudencagri@gmail.com>
2025-01-13 22:02:14 +0300

    Began

--- src/test/java/Pages/LocatorPage.java
+++ src/test/java/Pages/LocatorPage.java
@@ -1,10 +1,14 @@
 package Pages;
 
 import Utility.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LocatorPage extends  Parent{
     public LocatorPage() {
         PageFactory.initElements(GWD.getDriver(),this);
     }
+    @FindBy(xpath = "")
+    public WebElement first;
 }
